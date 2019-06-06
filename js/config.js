f8b17beb1f3d6513b7449d53f927aeae6d50e3d1
wjfrancis <william.francis.james@gmail.com>
2019-06-06 01:40:15 +0000

    configure congnito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_ryDjkE1TW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '54u34q38ut344o9ba77ufn50k2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
