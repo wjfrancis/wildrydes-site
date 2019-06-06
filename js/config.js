cf609245c67ebed4fba612df438ccdd82a9287e6
wjfrancis <william.francis.james@gmail.com>
2019-06-06 01:45:15 +0000

    config cognito again

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_ryDjkE1TW', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '54u34q38ut344o9ba77ufn50k2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
